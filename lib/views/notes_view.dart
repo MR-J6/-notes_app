e9b431c6082bd806f6ea7627aa52782f036a4ef9
MR-J6 <n3xuss666@gmail.com>
2026-04-02 17:01:33 +0200

    create note edit view

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -6,7 +6,7 @@ import 'package:notes_app/widgets/notes_list_view.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
-
+  static String id = 'NotesView';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/widgets/note_item.dart
+++ lib/widgets/note_item.dart
@@ -1,50 +1,56 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key, required this.color});
   final Color color;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 24),
-      margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-      height: 170,
-      width: 350,
-      decoration: BoxDecoration(
-        color: color,
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            contentPadding: EdgeInsets.zero,
-            title: Text(
-              'Flutter Tips',
-              style: TextStyle(fontSize: 24, color: Colors.black),
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(context, EditNoteView.id);
+      },
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 24),
+        margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+        height: 170,
+        width: 350,
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              contentPadding: EdgeInsets.zero,
+              title: Text(
+                'Flutter Tips',
+                style: TextStyle(fontSize: 24, color: Colors.black),
+              ),
+              subtitle: Text(
+                'Build your career with Mohamed Alaa',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black.withValues(alpha: 0.5),
+                ),
+              ),
+              trailing: Icon(
+                CupertinoIcons.delete,
+                color: Colors.black,
+                size: 30,
+              ),
             ),
-            subtitle: Text(
-              'Build your career with Mohamed Alaa',
+            Text(
+              'March 30, 2026',
               style: TextStyle(
                 fontSize: 16,
                 color: Colors.black.withValues(alpha: 0.5),
               ),
             ),
-            trailing: Icon(
-              CupertinoIcons.delete,
-              color: Colors.black,
-              size: 30,
-            ),
-          ),
-          Text(
-            'March 30, 2026',
-            style: TextStyle(
-              fontSize: 16,
-              color: Colors.black.withValues(alpha: 0.5),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
