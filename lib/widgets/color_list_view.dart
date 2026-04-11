ab1873535f6966d7d2db07f8fe8e2602110d044b
Mohamed <n3xuss666@gmail.com>
2026-04-11 16:49:08 +0200

    colors list view done

--- lib/widgets/color_list_view.dart
+++ lib/widgets/color_list_view.dart
@@ -1,26 +1,60 @@
 import 'package:flutter/material.dart';
 
 class ColorItem extends StatelessWidget {
-  const ColorItem({super.key});
-
+  const ColorItem({super.key, required this.isActive, required this.color});
+  final bool isActive;
+  final Color color;
   @override
   Widget build(BuildContext context) {
-    return const CircleAvatar(backgroundColor: Colors.purple, radius: 24);
+    return isActive
+        ? CircleAvatar(
+            backgroundColor: Colors.white,
+            radius: 24,
+            child: CircleAvatar(backgroundColor: color, radius: 20),
+          )
+        : CircleAvatar(backgroundColor: color, radius: 24);
   }
 }
 
-class ColorListView extends StatelessWidget {
+class ColorListView extends StatefulWidget {
   const ColorListView({super.key});
 
+  @override
+  State<ColorListView> createState() => _ColorListViewState();
+}
+
+class _ColorListViewState extends State<ColorListView> {
+  int currentIndex = 0;
+
+  final colors = const [
+    Color(0xffAC3931),
+    Color(0xffE5D352),
+    Color(0xffD9E76C),
+    Color(0xff537D8D),
+    Color(0xff482C3D),
+    Color.fromRGBO(103, 58, 183, 1),
+    Color.fromRGBO(255, 64, 129, 1),
+    Color.fromRGBO(68, 138, 255, 1),
+    Color.fromRGBO(105, 240, 174, 1),
+  ];
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       scrollDirection: Axis.horizontal,
-      itemCount: 10,
+      itemCount: colors.length,
       itemBuilder: (context, index) {
         return Padding(
           padding: const EdgeInsets.symmetric(horizontal: 8),
-          child: ColorItem(),
+          child: GestureDetector(
+            onTap: () {
+              currentIndex = index;
+              setState(() {});
+            },
+            child: ColorItem(
+              color: colors[index],
+              isActive: currentIndex == index,
+            ),
+          ),
         );
       },
     );
--- lib/widgets/notes_list_view.dart
+++ lib/widgets/notes_list_view.dart
@@ -7,13 +7,7 @@ import 'package:notes_app/widgets/note_item.dart';
 class NotesListView extends StatelessWidget {
   const NotesListView({super.key});
 
-  final colors = const [
-    Color.from(alpha: 0.9, red: 0.886, green: 0.843, blue: 0.251),
-    Colors.deepPurple,
-    Colors.pinkAccent,
-    Colors.blueAccent,
-    Colors.greenAccent,
-  ];
+  
 
   @override
   Widget build(BuildContext context) {
